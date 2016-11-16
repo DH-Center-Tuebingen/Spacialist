967b2225956329669d1bb3ca299be7033fe0147d
Benjamin Mitzkus <b.mitzkus@gmx.de>
2016-11-16 16:40:56 +0100

    Added thesaurus_val to table context_values

---
+++ lumen/database/migrations/2016_11_16_154245_add_thesaurus_val_on_context_values.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddThesaurusValOnContextValues extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('context_values', function (Blueprint $table) {
+            $table->text('thesaurus_val');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('context_values', function (Blueprint $table) {
+            $table->dropColumn('thesaurus_val');
+        });
+    }
+}
