f9770ea0535d81f7800f9378af92f791aec6f161
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2016-11-22 11:17:36 +0100

    added type column to bib_text table

---
+++ lumen/database/migrations/2016_11_22_100652_add_type_column_to_bib_tex.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTypeColumnToBibTex extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bib_text', function (Blueprint $table) {
+            $table->text('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bib_text', function (Blueprint $table) {
+            $table->dropColumn('type');
+        });
+    }
+}
