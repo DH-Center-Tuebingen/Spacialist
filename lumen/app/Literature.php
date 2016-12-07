bc17408a1c6063f6029a2a2d00933de73e8a6822
Benjamin Mitzkus <b.mitzkus@gmx.de>
2016-12-07 14:10:09 +0100

    Renamed table bib_tex to literature in the model, created migration

---
+++ lumen/app/Literature.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Literature extends Model
+{
+
+    protected $table = 'literature';
+    /**
+     * The attributes that are assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'author',
+        'editor',
+        'title',
+        'journal',
+        'year',
+        'pages',
+        'volume',
+        'number',
+        'booktitle',
+        'publisher',
+        'address',
+        'misc',
+        'howpublished',
+        'type',
+    ];
+}
