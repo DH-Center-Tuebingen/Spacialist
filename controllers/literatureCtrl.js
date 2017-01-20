1c057972fad07afcebd59a6e31bdc88201801a67
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2017-01-20 14:54:48 +0100

    check form data and send data to backend

--- controllers/literatureCtrl.js
+++ controllers/literatureCtrl.js
@@ -38,4 +38,38 @@ spacialistApp.controller('literatureCtrl', function($rootScope, $scope, scopeSer
         }
     ];
     $scope.literature.selectedType = $scope.availableTypes[0];
+
+    $scope.addLiterature = function(fields, type) {
+        if(typeof type == 'undefined') return;
+        if(typeof fields == 'undefined') return;
+        var mandatorySet = true;
+        for(var i=0; i<type.mandatoryFields.length; i++) {
+            var m = type.mandatoryFields[i];
+            if(typeof fields[m] == 'undefined') {
+                mandatorySet = false;
+                break;
+            }
+            if(fields[m].length === 0) {
+                mandatorySet = false;
+                break;
+            }
+        }
+        if(!mandatorySet) {
+            alert('Not all mandatory fields are set!');
+            return;
+        }
+        var formData = new FormData();
+        for(var field in fields) {
+            formData.append(field, fields[field]);
+        }
+        formData.append('type', type.name);
+        httpPostFactory('api/literature/add', formData, function(lit) {
+            if(lit.error) {
+                alert(lit.error);
+            } else {
+                scopeService.literature.push(lit.literature[0]);
+                $scope.literature.push(lit.literature[0]);
+            }
+        });
+    };
 });
