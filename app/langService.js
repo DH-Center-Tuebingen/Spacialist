be9af8e5b1ea980420a8227e4bdb3dc44e87370d
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2017-02-02 16:58:05 +0100

    seperate language logic into service and controller

---
+++ app/langService.js
@@ -0,0 +1,60 @@
+spacialistApp.service('langService', ['$translate', function($translate) {
+    var lang = {};
+
+    lang.availableLanguages = {
+        de: {
+            label: 'Deutsch',
+            flagCode: 'de'
+        },
+        en_UK: {
+            label: 'English (UK)',
+            flagCode: 'gb'
+        },
+        en_US: {
+            label: 'English (US)',
+            flagCode: 'us'
+        },
+        fr: {
+            label: 'Français',
+            flagCode: 'fr'
+        },
+        it: {
+            label: 'Italiano',
+            flagCode: 'it'
+        },
+        es: {
+            label: 'Español',
+            flagCode: 'es'
+        }
+    };
+
+    lang.setInitLanguage = function() {
+        updateLanguage($translate.resolveClientLocale());
+    };
+
+    var updateLanguage = function(langKey) {
+        if(typeof langKey == 'undefined') lang.currentLanguage = undefined;
+        else lang.currentLanguage = lang.availableLanguages[langKey];
+    };
+
+    lang.switchLang = function(key) {
+        var langPromise = $translate.use(key);
+        if(typeof langPromise == 'object') {
+            langPromise.then(function() { updateLanguage(key); });
+        } else {
+            updateLanguage(langPromise);
+        }
+    };
+
+    /*
+     * checks if the current language equals the param lang.
+     * Current language is in xx_XX format, but param lang can be xx as well
+     */
+    lang.isLangSet = function(lang) {
+        if(typeof lang.currentLanguage == 'undefined') return false;
+        if(typeof lang == 'undefined') return false;
+        return lang.currentLanguage == lang.availableLanguages[lang];
+    };
+
+    return lang;
+}]);
