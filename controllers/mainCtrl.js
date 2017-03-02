f4dcd9e7b5fdee5664e4861723252033f9bcabfe
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2017-03-02 17:12:42 +0100

    add geopgrahy form layout

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -183,4 +183,8 @@ spacialistApp.controller('mainCtrl', ['$rootScope', '$scope', 'userService', 'an
     $scope.openSourceModal = function(fieldname, fieldid, currentVal, currentDesc) {
         mainService.openSourceModal(fieldname, fieldid, currentVal, currentDesc);
     };
+
+    $scope.openGeographyPlacer = function() {
+        mainService.openGeographyModal($scope);
+    };
 }]);
