f0f108d8a375271bee1a207c52723d637c909afe
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2017-04-03 16:26:10 +0200

    add model for new photo_tag table

---
+++ lumen/app/PhotoTag.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PhotoTag extends Model
+{
+    /**
+     * The attributes that are assignable.
+     *
+     * @var array
+     */
+    protected $primaryKay = '';
+    protected $incrementing = false;
+}
