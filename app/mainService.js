142ef9cb6c128f1fe9dd079c833a206f3299f0cd
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2017-05-18 17:15:13 +0200

    only allow linking of geodata<=>contexts if their types match

--- app/mainService.js
+++ app/mainService.js
@@ -6,6 +6,7 @@ spacialistApp.service('mainService', ['httpGetFactory', 'httpGetPromise', 'httpP
         element: {},
         form: {},
         data: {},
+        geometryType: '',
         fields: {},
         sources: {}
     };
@@ -592,6 +593,9 @@ spacialistApp.service('mainService', ['httpGetFactory', 'httpGetPromise', 'httpP
         }
         var isCurrentlyLinked = mapService.geodata.linkedContexts[elem.geodata_id] && mapService.geodata.linkedContexts[elem.geodata_id] > 0;
         elem = target;
+        var layerId = mapService.geodata.linkedGeolayer[elem.context_type_id];
+        var layer = mapService.map.layers.overlays[layerId];
+        main.currentElement.geometryType = layer.layerOptions.type;
         console.log(elem);
         if(elem.typeid === 0) { //context
             elem.fields = main.contextReferences[elem.typename].slice();
