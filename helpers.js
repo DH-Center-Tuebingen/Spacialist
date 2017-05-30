86eb699747b9536f8db30e4ca5d23b45692642ac
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2017-05-30 15:09:39 +0200

    add helper functions for grid class detection

--- helpers.js
+++ helpers.js
@@ -4,3 +4,30 @@ Array.prototype.swap = function(a, b) {
     this[b] = t;
     return this;
 };
+
+function getCurrentDeviceClass() {
+    return $('#current-device-class').find('div:visible').first().attr('id');
+}
+
+function nextMatchingDeviceClass(col, currentClass) {
+    var availableClasses = ['xs', 'sm', 'md', 'lg'];
+    var indexedClasses = {
+        xs: undefined,
+        sm: undefined,
+        md: undefined,
+        lg: undefined
+    };
+    var regex = /col-([^-]*)-\d*/gi;
+    var matches = regex.exec(col);
+    while(matches !== null) {
+        var m = matches[1];
+        indexedClasses[m] = 1;
+        matches = regex.exec(col);
+    }
+    if(indexedClasses[currentClass]) return currentClass;
+    var startIndex = availableClasses.indexOf(currentClass);
+    for(var i=startIndex; i>=0; i--) {
+        if(indexedClasses[availableClasses[i]]) return availableClasses[i];
+    }
+    return null;
+}
