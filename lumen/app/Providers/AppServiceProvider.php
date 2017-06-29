f5f3d33a9da493ced83d1d9894b5924cf3a11851
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2017-06-29 14:55:37 +0200

    enable AppServiceProvider for custom validations

--- lumen/app/Providers/AppServiceProvider.php
+++ lumen/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,5 +15,11 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+
+    public function boot() {
+        // Validator::extend('', function ($attribute, $value, $parameters, $validator) {
+        //     return $value == '';
+        // });
     }
 }
--- lumen/bootstrap/app.php
+++ lumen/bootstrap/app.php
@@ -102,9 +102,9 @@ $app->routeMiddleware([
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
 //$app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 $app->register(Zizaco\Entrust\EntrustServiceProvider::class);
 $app->register(Phaza\LaravelPostgis\DatabaseServiceProvider::class);
