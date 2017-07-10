5b1b70dd2ec77164bf4a233702c6bc2ccdfb39b8
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2017-07-10 14:15:48 +0200

    fix add/delete of list entries

--- app/mainService.js
+++ app/mainService.js
@@ -441,6 +441,17 @@ spacialistApp.service('mainService', ['httpGetFactory', 'httpGetPromise', 'httpP
         }, '');
     };
 
+    main.addListEntry = function(index, inp, arr) {
+        if(typeof arr[index] == 'undefined') arr[index] = [];
+        arr[index].push({
+            'name': inp[index]
+        });
+    }
+
+    main.removeListItem = function(index, arr, $index) {
+        arr[index].splice($index, 1);
+    }
+
     function parseData(data) {
         var parsedData = {};
         for(var i=0; i<data.length; i++) {
