ea276ea7ad9aa632eed0808af57da483cc348461
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2017-08-19 13:09:08 +0200

    add built-in preferences with their default values

--- lumen/database/migrations/2017_07_20_144117_add_user_prefs.php
+++ lumen/database/migrations/2017_07_20_144117_add_user_prefs.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Preference;
 
 class AddUserPrefs extends Migration
 {
@@ -30,6 +31,52 @@ class AddUserPrefs extends Migration
             $table->foreign('pref_id')->references('id')->on('preferences')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
         });
+
+        $defaultPrefs = [
+            [
+                'label' => 'prefs.gui-language',
+                'default_value' => json_encode(['language_key' => 'en']),
+                'allow_override' => true
+            ],
+            [
+                'label' => 'prefs.columns',
+                'default_value' => json_encode(['left' => 2, 'center' => 5, 'right' => 5]),
+                'allow_override' => true
+            ],
+            [
+                'label' => 'prefs.show-tooltips',
+                'default_value' => json_encode(['show' => true]),
+                'allow_override' => true
+            ],
+            [
+                'label' => 'prefs.tag-root',
+                'default_value' => json_encode(['uri' => 'http://thesaurus.archeoinf.de/lukanien#fototyp']),
+                'allow_override' => false
+            ],
+            [
+                'label' => 'prefs.load-extensions',
+                'default_value' => json_encode(['map' => true, 'files' => true, 'bibliography' => true, 'data-analysis' => true]),
+                'allow_override' => false
+            ],
+            [
+                'label' => 'prefs.link-to-thesaurex',
+                'default_value' => json_encode(['show' => true]),
+                'allow_override' => false
+            ],
+            [
+                'label' => 'prefs.project-name',
+                'default_value' => json_encode(['name' => 'Spacialist']),
+                'allow_override' => false
+            ]
+        ];
+
+        foreach($defaultPrefs as $dp) {
+            $p = new Preference();
+            $p->label = $dp['label'];
+            $p->default_value = $dp['default_value'];
+            $p->allow_override = $dp['allow_override'];
+            $p->save();
+        }
     }
 
     /**
