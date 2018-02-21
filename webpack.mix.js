fceca82f096aa93fb37355df550585c146dfd8d2
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2018-02-21 10:38:13 +0100

    remove compiled files

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,7 +16,6 @@ mix.js('resources/assets/js/app.js', 'public/js')
    .copy('node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js', 'public/js')
    .copy('node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css', 'public/css')
    .copy('node_modules/bootstrap/dist/js/bootstrap.js', 'public/js')
-   .copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css')
    .copy('node_modules/vue-multiselect/dist/vue-multiselect.min.css', 'public/css')
    .autoload({
        jquery: ['$']
