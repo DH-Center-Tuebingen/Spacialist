96d73ee9c5fa4ceaa90feaaab4d27a0506a7d30c
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2018-03-12 11:18:20 +0100

    add pdf component

--- resources/assets/js/components/plugins/FilePdf.vue
+++ resources/assets/js/components/plugins/FilePdf.vue
@@ -1,7 +1,103 @@
 <template>
-    
+    <div>
+        <div>
+            <button type="button" class="btn btn-link" :disabled="isFirstPage" @click="gotoFirstPage()">
+                <i class="fas fa-fw fa-angle-double-left"></i>
+            </button>
+            <button type="button" class="btn btn-link" :disabled="isFirstPage" @click="gotoPreviousPage()">
+                <i class="fas fa-fw fa-angle-left"></i>
+            </button>
+            <button type="button" class="btn btn-link" :disabled="isLastPage" @click="gotoNextPage()">
+                <i class="fas fa-fw fa-angle-right"></i>
+            </button>
+            <button type="button" class="btn btn-link" :disabled="isLastPage" @click="gotoLastPage()">
+                <i class="fas fa-fw fa-angle-double-right"></i>
+            </button>
+            <button type="button" class="btn btn-link" @click="rotateCcw()">
+                <i class="fas fa-fw fa-undo-alt"></i>
+            </button>
+            <button type="button" class="btn btn-link" @click="rotateCw()">
+                <i class="fas fa-fw fa-redo-alt"></i>
+            </button>
+            <input type="text" style="max-width: 100px;" v-model.number="page" />/{{totalPages}}
+        </div>
+        <pdf class="modal-content-80"
+            :page="page || 1"
+            :rotate="rotation"
+            :src="file.url"
+            @num-pages="setNumberOfPages">
+        </pdf>
+    </div>
 </template>
 
 <script>
+    import pdf from 'vue-pdf';
 
+    export default {
+        components: {
+            pdf
+        },
+        props: {
+            file: {
+                required: true,
+                type: Object
+            }
+        },
+        mounted() {
+        },
+        methods: {
+            setNumberOfPages(pages) {
+                this.totalPages = pages;
+            },
+            gotoFirstPage() {
+                this.page = 1;
+            },
+            gotoPreviousPage() {
+                if(this.page > 1) {
+                    this.page--;
+                }
+            },
+            gotoNextPage() {
+                if(this.page < this.totalPages) {
+                    this.page++;
+                }
+            },
+            gotoLastPage() {
+                this.page = this.totalPages;
+            },
+            rotateCcw() {
+                if(this.rotationIndex == 0) {
+                    this.rotationIndex = this.rotations.length - 1;
+                } else {
+                    this.rotationIndex--;
+                }
+            },
+            rotateCw() {
+                if(this.rotationIndex == this.rotations.length - 1) {
+                    this.rotationIndex = 0;
+                } else {
+                    this.rotationIndex++;
+                }
+            }
+        },
+        data() {
+            return {
+                rotations: [0, 90, 180, 270],
+                rotationIndex: 0,
+                page: 1,
+                totalPages: 1
+            }
+        },
+        computed: {
+            rotation: function() {
+                return this.rotations[this.rotationIndex];
+            },
+            isFirstPage: function() {
+                return this.page == 1;
+            },
+            isLastPage: function() {
+                return this.page == this.totalPages;
+            }
+        }
+    }
 </script>
