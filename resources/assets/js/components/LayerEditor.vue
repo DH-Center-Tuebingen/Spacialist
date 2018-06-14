7a668f7ea0c224188fe792568d5e7bc176c1488f
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2018-06-14 15:48:09 +0200

    fix style and scrolling; correctly update layer props after patch/update

--- resources/assets/js/components/LayerEditor.vue
+++ resources/assets/js/components/LayerEditor.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="row">
-        <div class="col-md-3">
+    <div class="row h-100">
+        <div class="col-md-3 h-100 d-flex flex-column">
             <h5>Baselayer</h5>
             <layer
+                class="flex-grow-1 scroll-y-auto"
                 :add-new="onAddNewBaselayer"
                 :concepts="concepts"
                 :layer="baselayer"
@@ -10,155 +11,158 @@
             </layer>
             <h5>Overlays</h5>
             <layer
+                class="flex-grow-1 scroll-y-auto"
                 :add-new="onAddNewOverlay"
                 :concepts="concepts"
                 :layer="overlays"
                 :on-select="onLayerSelect">
             </layer>
         </div>
-        <div class="col-md-9">
+        <div class="col-md-6 h-100">
             <p class="alert alert-info" v-if="!selectedLayer.id">
                 Please select a layer
             </p>
-            <div v-else>
-                <h5>Properties of {{ getTitle(selectedLayer) }}</h5>
-                <form role="form" @submit.prevent="updateLayer(selectedLayer, originalLayer)">
+            <div v-else class="h-100 d-flex flex-column">
+                <div class="d-flex flex-row justify-content-between mb-2">
+                    <h5>Properties of {{ getTitle(selectedLayer) }}</h5>
+                    <button type="submit" form="layer-form" class="btn btn-success">
+                        <i class="fas fa-fw fa-save"></i> Save
+                    </button>
+                </div>
+                <form role="form" id="layer-form" class="col pl-0 scroll-y-auto scroll-x-hidden" @submit.prevent="updateLayer(selectedLayer, originalLayer)">
                     <div v-if="selectedLayer.context_type_id || selectedLayer.type == 'unlinked'">
                         <div class="form-group row">
-                            <label for="color" class="col-md-2 col-form-label">
+                            <label for="color" class="col-md-4 col-form-label">
                                 Color:
                             </label>
-                            <div class="col-md-4">
+                            <div class="col-md-8">
                                 <input type="color" id="color" class="form-control" v-model="selectedLayer.color" />
                             </div>
                         </div>
                     </div>
                     <div v-else>
                         <div class="form-group row">
-                            <label for="name" class="col-md-2 col-form-label">
+                            <label for="name" class="col-md-4 col-form-label">
                                 Name:
                             </label>
-                            <div class="col-md-4">
+                            <div class="col-md-8">
                                 <input type="text" id="name" class="form-control" v-model="selectedLayer.name" />
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="url" class="col-md-2 col-form-label">
+                            <label for="url" class="col-md-4 col-form-label">
                                 URL:
                             </label>
-                            <div class="col-md-4">
+                            <div class="col-md-8">
                                 <input type="text" id="url" class="form-control" v-model="selectedLayer.url" />
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="type" class="col-md-2 col-form-label">
+                            <label for="type" class="col-md-4 col-form-label">
                                 Type:
                             </label>
-                            <div class="col-md-4">
+                            <div class="col-md-8">
                                 <input type="text" id="type" class="form-control" v-model="selectedLayer.type" />
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="subdomains" class="col-md-2 col-form-label">
+                            <label for="subdomains" class="col-md-4 col-form-label">
                                 Subdomains:
                             </label>
-                            <div class="col-md-4">
+                            <div class="col-md-8">
                                 <input type="text" id="subdomains" class="form-control" v-model="selectedLayer.subdomains" />
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="attribution" class="col-md-2 col-form-label">
+                            <label for="attribution" class="col-md-4 col-form-label">
                                 Attribution:
                             </label>
-                            <div class="col-md-4">
+                            <div class="col-md-8">
                                 <textarea id="attribution" class="form-control" v-model="selectedLayer.attribution"></textarea>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="layers" class="col-md-2 col-form-label">
+                            <label for="layers" class="col-md-4 col-form-label">
                                 Layers:
                             </label>
-                            <div class="col-md-4">
+                            <div class="col-md-8">
                                 <input type="text" id="layers" class="form-control" v-model="selectedLayer.layers" />
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="styles" class="col-md-2 col-form-label">
+                            <label for="styles" class="col-md-4 col-form-label">
                                 Styles:
                             </label>
-                            <div class="col-md-4">
+                            <div class="col-md-8">
                                 <textarea id="styles" class="form-control" v-model="selectedLayer.styles"></textarea>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="format" class="col-md-2 col-form-label">
+                            <label for="format" class="col-md-4 col-form-label">
                                 Format:
                             </label>
-                            <div class="col-md-4">
+                            <div class="col-md-8">
                                 <textarea id="format" class="form-control" v-model="selectedLayer.format"></textarea>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="version" class="col-md-2 col-form-label">
+                            <label for="version" class="col-md-4 col-form-label">
                                 Version:
                             </label>
-                            <div class="col-md-4">
+                            <div class="col-md-8">
                                 <input type="text" id="version" class="form-control" v-model="selectedLayer.version" />
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="overlay" class="col-md-2 col-form-label">
+                            <label for="overlay" class="col-md-4 col-form-label">
                                 Overlay:
                             </label>
-                            <div class="col-md-4">
+                            <div class="col-md-8">
                                 <input type="checkbox" id="overlay" class="form-control" v-model="selectedLayer.is_overlay" />
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="api-key" class="col-md-2 col-form-label">
+                            <label for="api-key" class="col-md-4 col-form-label">
                                 API-Key:
                             </label>
-                            <div class="col-md-4">
+                            <div class="col-md-8">
                                 <input type="text" id="api-key" class="form-control" v-model="selectedLayer.api_key" />
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="layer-type" class="col-md-2 col-form-label">
+                            <label for="layer-type" class="col-md-4 col-form-label">
                                 Layer Type:
                             </label>
-                            <div class="col-md-4">
+                            <div class="col-md-8">
                                 <input type="text" id="layer-type" class="form-control" v-model="selectedLayer.layer_type" />
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="color" class="col-md-2 col-form-label">
+                            <label for="color" class="col-md-4 col-form-label">
                                 Color:
                             </label>
-                            <div class="col-md-4">
+                            <div class="col-md-8">
                                 <input type="color" id="color" class="form-control" v-model="selectedLayer.color" />
                             </div>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="visible" class="col-md-2 col-form-label">
+                        <label for="visible" class="col-md-4 col-form-label">
                             Visible:
                         </label>
-                        <div class="col-md-4">
+                        <div class="col-md-8">
                             <input type="checkbox" id="visible" class="form-control" v-model="selectedLayer.visible" />
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="opacity" class="col-md-2 col-form-label">
+                        <label for="opacity" class="col-md-4 col-form-label">
                             Opacity:
                         </label>
-                        <div class="col-md-4 d-flex">
+                        <div class="col-md-8 d-flex">
                             <input type="range" id="opacity" class="form-control" step="0.01" min="0" max="1" value="0" v-model="selectedLayer.opacity"/>
                             <span class="ml-3">{{ selectedLayer.opacity }}</span>
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-success">
-                        <i class="fas fa-fw fa-save"></i> Save
-                    </button>
                 </form>
             </div>
         </div>
@@ -230,11 +234,31 @@
                 }
                 vm.$http.patch(`/api/map/layer/${lid}`, data).then(function(response) {
                     let activeBaseLayerChanged = false;
+                    let layerGroup;
                     if(!tmpLayer.is_overlay) {
+                        layerGroup = vm.baselayer;
                         activeBaseLayerChanged = tmpLayer.visible != orgLayer.visible;
-                        // TODO update visible state of former active base layer
+                        // Only update other visible states, if this one
+                        // is now visible
+                        if(activeBaseLayerChanged && tmpLayer.visible) {
+                            vm.baselayer.forEach(bl => {
+                                if(bl.id != tmpLayer.id) {
+                                    bl.visible = false;
+                                }
+                            });
+                        }
+                    } else {
+                        layerGroup = vm.overlays;
                     }
-                    // TODO update layer props
+                    let updateLayer = layerGroup.find(l => l.id == orgLayer.id);
+                    if(updateLayer) {
+                        for(let k in tmpLayer) {
+                            if(updateLayer[k] != tmpLayer[k]) {
+                                updateLayer[k] = tmpLayer[k];
+                            }
+                        }
+                    }
+                    vm.$showToast('Layer updated', `Layer ${layer.name} successfully updated.`, 'success');
                 }).catch(function(error) {
                     vm.$throwError(error);
                 });
