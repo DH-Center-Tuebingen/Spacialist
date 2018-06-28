aa5b955989695e7a7ba2792a7292e3f3bb0bdcb9
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2018-06-28 14:53:57 +0200

    switch tree plugin (using patched version, see #55 in project)

--- resources/assets/js/components/ContextTree.vue
+++ resources/assets/js/components/ContextTree.vue
@@ -1,17 +1,30 @@
 <template>
     <div class="container">
-        <tree-view
-            :category="category"
-            :css="css"
-            :display="display"
-            :dragndrop="dragndrop"
-            :model="tree"
-            :onSelect="onSelect"
-            :openerOpts="openerOpts"
-            :search="search"
-            :selection="selection"
-            :strategies="strategies">
-        </tree-view>
+        <v-jstree
+            :async="loadAsync"
+            :data="tree"
+            :draggable="true"
+            :drag-over-background-color="dragOverColor"
+            :item-events="itemEvents"
+            @item-click="itemClick"
+            @item-toggle="itemToggle"
+            @item-drag-start="itemDragStart"
+            @item-drag-enter="itemDragEnter"
+            @item-drag-leave="itemDragLeave"
+            @item-drag-end="itemDragEnd"
+            @item-drop-before="itemDropBefore"
+            @item-drop="itemDrop">
+            <div slot-scope="_">
+                <i class="fas fa-fw fa-spa"></i>
+                <span>{{_.model.name}}</span>
+                <span class="pl-1 font-italic mb-0" v-if="_.model.context_type_id">
+                    {{ $translateConcept($getEntityType(_.model.context_type_id).thesaurus_url) }}
+                </span>
+                <span class="pl-1" v-show="_.model.children_count">
+                    ({{ _.model.children_count }})
+                </span>
+            </div>
+        </v-jstree>
 
         <vue-context ref="contextMenu" class="context-menu-wrapper">
             <ul class="list-group list-group-vue-context" slot-scope="itemScope" v-if="itemScope.data">
@@ -33,17 +46,32 @@
 </template>
 
 <script>
-    import { TreeView } from '@bosket/vue';
-    import { dragndrop } from "@bosket/core";
-    import { array } from '@bosket/tools';
+    import VJstree from 'vue-jstree';
     import { VueContext } from 'vue-context';
     import { transliterate as tr, slugify } from 'transliteration';
 
     class Node {
         constructor(data) {
             Object.assign(this, data);
+            this.opened = false;
+            this.selected = false;
+            this.disabled = false;
+            this.loading = false;
+            this.isLeaf = false;
+            this.dropDisabled = false;
+            this.dragDisabled = false;
             if(this.children_count > 0) {
-                this.children = () => this.fetchChildren(this.id);
+                this.children = [{
+                    name: 'Loading...',
+                    opened: false,
+                    selected: false,
+                    disabled: true,
+                    loading: true,
+                    children: []
+                }];
+                this.asyncChildren = () => this.fetchChildren(this.id);
+            } else {
+                this.children = [];
             }
         }
 
@@ -53,15 +81,17 @@
                 const result = response.data.map(n => new Node({...n, parent: this}));
                 return result;
             }).catch(function(error) {
-                vm.$throwError(error);
+                log.error("FIX ME");
+                // TODO neither vm nor this nor Vue are accessible here
+                // vm.$throwError(error);
             });
         }
     }
 
     export default {
         components: {
-            'tree-view': TreeView,
-            VueContext
+            VueContext,
+            VJstree
         },
         props: {
             onContextMenuAdd: {
@@ -83,18 +113,119 @@
             selectionCallback: {
                 required: false,
                 type: Function
+            },
+            dragDelay: {
+                required: false,
+                type: Number,
+                default: 500
             }
         },
         mounted() {
             this.init();
         },
         methods: {
-            init() {
-                this.tree = this.roots.map(n => new Node(n));
+            itemClick(node, item, e) {
+                this.selectionCallback(item);
             },
-            onSelect(newSelection) {
-                this.selection = newSelection
-                this.selectionCallback(newSelection[0]);
+            itemToggle(node, item, e) {
+            },
+            itemDragStart(node, item, e) {
+                this.dragging = true;
+            },
+            itemDragEnter(node, item, draggedItem, e) {
+                const vm = this;
+                if(vm.dragDelayId) {
+                    clearTimeout(vm.dragDelayId);
+                }
+                item.dropDisabled = !vm.isDropAllowed(item, draggedItem);
+                if(item.dropDisabled) {
+                    vm.dragOverColor = '#FDC9C9';
+                } else {
+                    vm.dragOverColor = '#C9FDC9'
+                }
+                vm.dragDelayId = setTimeout(function() {
+                    if(!item.opened && item.children_count) {
+                        item.opened = true;
+                    }
+                    vm.dragDelayId = 0;
+                }, vm.dragDelay);
+            },
+            itemDragLeave(node, item, draggedItem, e) {
+            },
+            itemDragEnd(node, item, e) {
+                this.dragging = false;
+            },
+            itemDropBefore(node, item, draggedItem, e) {
+            },
+            itemDrop(node, item, draggedItem, e) {
+                const vm = this;
+                const rank = item.children ? item.children.length + 1 : 1;
+                let data = {
+                    rank: rank
+                };
+                if(!item.isRootNode) {
+                    data.parent_id = item.id;
+                }
+
+                vm.$http.patch(`/api/context/${draggedItem.id}/rank`, data).then(function(response) {
+                    // Because dropped elements are added at the end,
+                    // we do not have to update the local tree and
+                    // simply use array length as rank
+                });
+            },
+            loadAsync(node, resolve) {
+                if(!node.data.id || !node.data.asyncChildren) {
+                    // resolve([]);
+                } else {
+                    const id = node.data.id;
+                    node.data.asyncChildren().then(function(response) {
+                        resolve(response);
+                    });
+                }
+            },
+            init() {
+                this.tree = [{
+                    name: 'Root Node',
+                    opened: true,
+                    selected: false,
+                    disabled: true,
+                    loading: false,
+                    isLeaf: false,
+                    dropDisabled: false,
+                    dragDisabled: true,
+                    isRootNode: true,
+                    children: this.roots.map(n => new Node(n))
+                }];
+            },
+            isDropAllowed(tgtItem, item) {
+                const vm = this;
+                const dragContextType = vm.$getEntityType(item.context_type_id);
+                let dropContextType;
+                if(tgtItem.isRootNode) {
+                    dropContextType = {
+                        sub_context_types: Object.values(vm.$getEntityTypes()).filter(f => f.is_root)
+                    }
+                } else {
+                    dropContextType = vm.$getEntityType(tgtItem.context_type_id);
+                }
+                // If currently dragged element is not allowed as root
+                // and dragged on element is a root element (no parent)
+                // do not allow drop
+                if(!dragContextType.is_root && tgtItem.isRootNode) {
+                    return false;
+                }
+
+                // Check if currently dragged context type is allowed
+                // as subtype of current drop target
+                const index = dropContextType.sub_context_types.findIndex(ct => ct.id == dragContextType.id);
+                if(index == -1) {
+                    return false;
+                }
+                // In any other cases allow drop
+                return true;
+            },
+            treeContextMenuEvent(node, item, e) {
+                this.openContextMenu(e, item);
             },
             openContextMenu(e, item) {
                 this.$refs.contextMenu.open(e, { item: item });
@@ -104,136 +235,12 @@
         data() {
             return {
                 tree: [],
-                selection: [],
-                dragndrop: {
-                    ...dragndrop.selection(() => this.tree, m => this.tree = m),
-                    over: (target, event, inputs) => {
-                        // TODO open on hover
-                    },
-                    // Returns true if element is guarded (drop not allowed)
-                    guard: (target, event, inputs) => {
-                        const vm = this;
-                        const dragElem = inputs.selection[0];
-                        const dragContextType = vm.$getEntityType(dragElem.context_type_id);
-                        const dropContextType = vm.$getEntityType(target.context_type_id);
-                        // If currently dragged element is not allowed as root
-                        // and dragged on element is a root element (no parent)
-                        // do not allow drop
-                        if(!dragContextType.is_root && (!target.parent /*&& folded*/)) {
-                            return true;
-                        }
-
-                        // Check if currently dragged context type is allowed
-                        // as subtype of current drop target
-                        const index = dropContextType.sub_context_types.findIndex(ct => ct.id == dragElem.context_type_id);
-                        if(index == -1) {
-                            return true;
-                        }
-
-                        // In any other cases allow drop
-                        return false;
-                    },
-                    drop: (target, event, inputs) => {
-                        const vm = this;
-                        const dragElem = inputs.selection[0];
-                        let children;
-                        let parentId;
-                        let oldParentId;
-                        if(target.children /*&& !folded*/) { // TODO
-                            children = target.children;
-                            parentId = target.id;
-                        } else if(target.parent) {
-                            children = target.parent.children;
-                            parentId = target.parent.id;
-                        } else {
-                            children = vm.tree;
-                        }
-                        if(dragElem.parent) {
-                            oldParentId = dragElem.parent.id;
-                        }
-
-                        let newIndex = children.indexOf(target) + 1;
-                        const oldIndex = dragElem.rank - 1;
-
-                        // Check if element was moved (different parent OR different index)
-                        if((!parentId && !oldParentId) || parentId == oldParentId) {
-                            if(newIndex == oldIndex) return;
-                            if(oldIndex < newIndex) newIndex--;
-                        }
-
-                        let rank = newIndex + 1;
-                        let data = {
-                            rank: rank
-                        };
-                        if(parentId) {
-                            data.parent_id = parentId;
-                        }
-
-                        vm.$http.patch(`/api/context/${dragElem.id}/rank`, data).then(function(response) {
-                            let oldChildren;
-                            if(parentId != oldParentId) {
-                                if(oldParentId) {
-                                    oldChildren = dragElem.parent.children;
-                                } else {
-                                    oldChildren = vm.tree;
-                                }
-                                for(let i=oldIndex; i<oldChildren.length; i++) {
-                                    oldChildren[i].rank--;
-                                }
-                                for(let i=newIndex; i<children.length; i++) {
-                                    children[i].rank++;
-                                }
-                            } else {
-                                oldChildren = children;
-                                if(newIndex < oldIndex) {
-                                    for(let i=newIndex; i<oldIndex; i++) {
-                                        children[i].rank++;
-                                    }
-                                } else {
-                                    for(let i=oldIndex; i<newIndex; i++) {
-                                        children[i].rank--;
-                                    }
-                                }
-                            }
-
-                            const index = oldChildren.findIndex(c => c.id == dragElem.id);
-                            if(index > -1) {
-                                oldChildren.splice(index, 1);
-                            }
-                            dragElem.rank = newIndex + 1;
-                            children.splice(newIndex, 0, dragElem);
-                        }).catch(function(error) {
-                            vm.$throwError(error);
-                        });
-                    }
+                dragging: false,
+                dragOverColor: '#C9FDC9',
+                itemEvents: {
+                    contextmenu: this.treeContextMenuEvent
                 },
-                strategies: {
-                    selection: ["single"],
-                    click: ["select"],
-                    fold: ["opener-control"]
-                },
-                openerOpts: {
-                    position: 'left'
-                },
-                css: {
-                    opener: 'opener mr-2 text-info',
-                    search: 'form-control'
-                },
-                category: "children",
-                search: input => {
-                    input = tr(input);
-                    return item => tr(item.name).match(new RegExp(`.*${ input }.*`, "gi"))
-                },
-                display: (item, inputs) =>
-                    <span onContextmenu={($event) => this.openContextMenu($event, item)}>
-                        <span>{item.name}</span>
-                        <span class="pl-2 font-italic mb-0">
-                            {this.$translateConcept(this.$getEntityType(item.context_type_id).thesaurus_url)}
-                        </span>
-                        <span class="pl-2">
-                            {item.children_count > 0 ? `(${item.children_count})` : ""}
-                        </span>
-                    </span>
+                dragDelayId: 0
             }
         }
     }
