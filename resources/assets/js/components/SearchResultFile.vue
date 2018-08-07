e71a835a74e069722b764084b184c5a3c4ec90f5
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2018-08-07 11:12:39 +0200

    remove logging

--- resources/assets/js/components/SearchResultFile.vue
+++ resources/assets/js/components/SearchResultFile.vue
@@ -22,9 +22,7 @@
                 type: Object
             },
         },
-        mounted () {
-            console.log(this.data);
-        },
+        mounted () {},
         methods: {
         },
         data () {
