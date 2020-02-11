c2349350ae96426a97bef67feec8851413e0e992
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2020-02-11 15:02:29 +0100

    add middleware to automatically lower certain string inputs

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
+        \App\Http\Middleware\LowerStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\LanguageMiddleware::class,
---
+++ app/Http/Middleware/src/LowerStrings.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Middleware\Src;
+
+use Illuminate\Foundation\Http\Middleware\TransformsRequest;
+
+class LowerStrings extends TransformsRequest
+{
+    /**
+     * The attributes that should not be lowered. Ignored if $include is set.
+     *
+     * @var array
+     */
+    protected $except = [
+        //
+    ];
+
+    /**
+     * The attributes that should be lowered.
+     *
+     * @var array
+     */
+    protected $include = [
+        //
+    ];
+
+    /**
+     * Transform the given value.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return mixed
+     */
+    protected function transform($key, $value)
+    {
+        if (count($this->include) > 0) {
+            if (!in_array($key, $this->include, true)) {
+                return $value;
+            }
+        } else {
+            if (in_array($key, $this->except, true)) {
+                return $value;
+            }
+        }
+
+        return is_string($value) ? strtolower($value) : $value;
+    }
+}
