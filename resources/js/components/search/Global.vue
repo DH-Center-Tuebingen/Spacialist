3c904668dcbba1b1ce38b36584dbdcdbceeadbcf
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2021-03-02 11:49:35 +0100

    polish global search comp
    Signed-off-by: Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>

--- resources/js/components/search/Global.vue
+++ resources/js/components/search/Global.vue
@@ -2,6 +2,7 @@
     <multiselect
         v-model="state.entry"
         name="global-search"
+        class="multiselect-search"
         :object="false"
         :label="'id'"
         :track-by="'id'"
@@ -10,6 +11,9 @@
         :options="query => search(query)"
         :filterResults="false"
         :resolveOnLoad="false"
+        :clearOnSearch="true"
+        :clearOnSelect="true"
+        :caret="false"
         :minChars="0"
         :searchable="true"
         :delay="delay"
@@ -27,14 +31,7 @@
 </template>
 
 <script>
-    // import TypeaheadSearch from './TypeaheadSearch.vue';
-
-    // import SearchResultBibliography from './SearchResultBibliography.vue';
-    // import SearchResultEntity from './SearchResultEntity.vue';
-    // import SearchResultFile from './SearchResultFile.vue';
-    // import SearchResultGeodata from './SearchResultGeodata.vue';
     import {
-        computed,
         reactive,
         toRefs,
     } from 'vue';
@@ -93,91 +90,5 @@
                 state,
             };
         },
-        // methods: {
-        //     update() {
-        //         this.cancel();
-
-        //         if(!this.query) {
-        //             return this.reset();
-        //         }
-
-        //         if(this.minChars) {
-        //             if(this.query.length < this.minChars) {
-        //                 return;
-        //             }
-        //             if(this.hasShebang && this.query.length - this.shebangLength < this.minChars) {
-        //                 return;
-        //             }
-        //         }
-
-        //         this.loading = true;
-
-        //         this.fetch().then((response) => {
-        //             if(response && this.query) {
-        //                 let data = response.data;
-        //                 data = this.prepareResponseData ? this.prepareResponseData(data) : data;
-        //                 this.items = data;
-        //                 this.current = -1;
-        //                 this.loading = false;
-
-        //                 if(this.selectFirst) {
-        //                     this.down();
-        //                 }
-        //             }
-        //         });
-        //     },
-        //     onHit(item) {
-        //         if(item) {
-        //             this.query = item.name;
-        //             switch (item.group) {
-        //                 case 'entities':
-        //                     this.$router.push({
-        //                         name: 'entitydetail',
-        //                         params: {
-        //                             id: item.id
-        //                         }
-        //                     });
-        //                     break;
-        //                 case 'files':
-        //                     const currRoute = this.$route;
-        //                     const query = {
-        //                         tab: 'files',
-        //                         f: item.id
-        //                     };
-        //                     // Only append, if current route is one of the
-        //                     // MainView routes
-        //                     const append = currRoute.name == 'home' || currRoute.name == 'entitydetail';
-        //                     this.$router.push({
-        //                         append: append,
-        //                         query: query
-        //                     });
-        //                     break;
-        //                 case 'bibliography':
-        //                     this.$router.push({
-        //                         name: 'bibedit',
-        //                         params: {
-        //                             id: item.id
-        //                         }
-        //                     });
-        //                     break;
-        //                 case 'geodata':
-        //                     //TODO
-        //                     break;
-        //                 default:
-        //                     this.$throwError({message: `Action is not yet implemented for items of type ${item.group}.`});
-        //             }
-        //         } else {
-        //             this.query = '';
-        //         }
-        //         this.closeSelect();
-        //     },
-        //     clearItem() {
-        //         this.onHit();
-        //     },
-        //     closeSelect() {
-        //         this.items = [];
-        //         this.loading = false;
-        //     }
-        // }
     }
 </script>
