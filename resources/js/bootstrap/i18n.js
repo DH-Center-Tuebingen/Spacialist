17e50316d43f291a558cdf24013abd59e5f4a057
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2021-09-15 12:28:28 +0200

    convert i18n files to json to allow external translations
    Signed-off-by: Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>

--- resources/js/bootstrap/i18n.js
+++ resources/js/bootstrap/i18n.js
@@ -1,6 +1,6 @@
 import { createI18n } from 'vue-i18n';
-import en from '../i18n/en';
-import de from '../i18n/de';
+import * as en from '../i18n/en.json';
+import * as de from '../i18n/de.json';
 
 const messages = {
     en: en,
