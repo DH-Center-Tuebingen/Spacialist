7df3bc733872751ab35a50ac2c9a92723b8fd8a6
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2022-03-22 15:18:05 +0100

    add japanese i18n files; fix missing/wrong values
    Signed-off-by: Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>

--- resources/js/bootstrap/i18n.js
+++ resources/js/bootstrap/i18n.js
@@ -1,10 +1,12 @@
 import { createI18n } from 'vue-i18n';
 import * as en from '../i18n/en.json';
 import * as de from '../i18n/de.json';
+import * as ja from '../i18n/ja.json';
 
 const messages = {
     en: en,
-    de: de
+    de: de,
+    ja: ja,
 }
 const i18n = createI18n({
     legacy: false,
