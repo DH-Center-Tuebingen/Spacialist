880d29c7530f995fc24e1385163e870d1cf2eec1
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2023-10-17 13:11:16 +0200

    fix multiselect checkboxes
    Signed-off-by: Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>

--- resources/js/components/tree/Node.vue
+++ resources/js/components/tree/Node.vue
@@ -1,39 +1,16 @@
 <template>
-    <div
-        :id="`tree-node-${data.id}`"
-        @dragenter="onDragEnter"
-        @dragleave="onDragLeave"
-    >
-        <input
-            v-show="state.isSelectionMode"
-            :id="`tree-node-mes-${data.id}`"
-            v-model="state.multieditSelected"
-            class="mx-1 form-check-input"
-            type="checkbox"
-            :disabled="state.isSelectionDisabled"
-            @click.stop="addToMSList()"
-        >
-        <a
-            :id="`tree-node-cm-toggle-${data.id}`"
-            href=""
-            class="text-body text-decoration-none disabled"
-            data-bs-toggle="dropdown"
-            data-bs-auto-close="true"
-            aria-expanded="false"
-            @click.prevent
-            @contextmenu.stop.prevent="togglePopup()"
-        >
-            <span
-                style="display: inline-block; text-align: center;"
-                class="px-1"
-            >
-                <span
-                    v-if="data.children_count"
-                    class="badge rounded-pill"
-                    style="font-size: 9px;"
-                    :style="state.colorStyles"
-                    :title="data.children_count"
-                >
+    <div @dragenter="onDragEnter" @dragleave="onDragLeave" :id="`tree-node-${data.id}`" @click="e => addToMSList(e)">
+        <span v-show="state.isSelectionMode" class="mx-1">
+            <span v-show="state.multieditSelected" class="text-success">
+                <i class="fas fa-fw fa-circle-check"></i>
+            </span>
+            <span v-show="!state.multieditSelected">
+                <i class="far fa-fw fa-circle"></i>
+            </span>
+        </span>
+        <a href="" :id="`tree-node-cm-toggle-${data.id}`" @click.prevent @contextmenu.stop.prevent="togglePopup()" class="text-body text-decoration-none disabled" data-bs-toggle="dropdown" data-bs-auto-close="true" aria-expanded="false">
+            <span style="display: inline-block; text-align: center;" class="px-1">
+                <span v-if="data.children_count" class="badge rounded-pill" style="font-size: 9px;" :style="state.colorStyles" :title="data.children_count">
                     {{ numPlus(data.children_count, 3) }}
                 </span>
                 <span
@@ -215,7 +192,11 @@ export default {
         const onDragLeave = _ => {
 
             };
-            const addToMSList = _ => {
+            const addToMSList = event => {
+                if(!state.isSelectionMode) return;
+
+                event.stopPropagation();
+                event.preventDefault();
                 state.multieditSelected = !state.multieditSelected;
                 if(state.multieditSelected) {
                     store.dispatch('addToTreeSelection', {
