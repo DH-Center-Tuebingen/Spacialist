de87122930edf988bae1fdf1c1c88e0700cbe7e3
Severino <severin.opel@outlook.com>
2023-10-25 18:06:00 +0200

    make eslint focus vue/no-v-html directly

--- resources/js/components/preferences/ResetEmail.vue
+++ resources/js/components/preferences/ResetEmail.vue
@@ -18,12 +18,12 @@
             class="row mt-3"
         >
             <div class="col-md-10 offset-md-2">
-                <!-- eslint-disable -->
+                <!-- eslint-disable vue/no-v-html -->
                 <div
                     class="alert bg-info mb-0 w-50"
                     v-html="t('main.preference.info.password_reset_link')"
                 />
-                <!-- eslint-enable -->
+                <!-- eslint-enable vue/no-v-html -->
             </div>
         </div>
     </div>
