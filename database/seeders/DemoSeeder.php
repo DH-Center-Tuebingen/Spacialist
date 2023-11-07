2e12d6b3b6de8238079f3e69e3719a347d1fa03c
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2023-11-07 15:56:20 +0100

    remove seed part already done in migration
    Signed-off-by: Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>

--- database/seeders/DemoSeeder.php
+++ database/seeders/DemoSeeder.php
@@ -26,7 +26,7 @@ class DemoSeeder extends Seeder
         $this->call(ThConceptNotesTableSeeder::class);
         $this->call(ThConceptNotesMasterTableSeeder::class);
 
-        $this->call(PermissionsTableSeeder::class);
+        // $this->call(PermissionsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(PermissionRoleTableSeeder::class);
         $this->call(RoleUserTableSeeder::class);
