2167b5523e60819e577993dbf7a82f758a101382
Severino <severin.opel@hotmail.com>
2024-01-25 17:38:25 +0100

    Reworked command palette

--- resources/js/components/mde/CommandPalette.vue
+++ resources/js/components/mde/CommandPalette.vue
@@ -1,145 +1,45 @@
 <template>
-    <div
-        class="d-flex flex-row gap-2 align-items-center bg-primary bg-opacity-10 px-2 py-1"
-    >
-        <span
-            @click="undo()"
-        >
-            <i class="fas fa-fw fa-undo" />
-        </span>
-        <span
-            @click="redo()"
-        >
-            <i class="fas fa-fw fa-redo" />
-        </span>
-        |
-        <div
-            class="d-flex flex-row"
-        >
+    <div class="d-flex flex-row gap-2 align-items-center bg-primary bg-opacity-10 px-2 py-1 user-select-none">
+        <!-- COMMAND UNDO REDO -->
+        <template v-for="(commandGroup, index) of commandGroups">
             <span
-                @click="heading(1)"
+                v-for="command of commandGroup"
+                :key="command.name"
+                class="clickable"
+                :class="getClassFromCommand(command)"
+                @click="command.command"
             >
-                <i class="fas fa-fw fa-h" />
-            </span>
-            <div class="dropdown">
-                <span
-                    class="dropdown-toggle"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
+
+                <template v-if="Array.isArray(command.icon)">
+                    <i
+                        v-for="icon of command.icon"
+                        :key="icon"
+                        :class="icon"
+                    />
+                </template>
+                <i
+                    v-else
+                    :class="command.icon"
                 />
-                <ul class="dropdown-menu small">
-                    <li>
-                        <span
-                            class="dropdown-item text-muted d-flex flex-row gap-1"
-                            @click="heading(1)"
-                        >
-                            <i class="fas fa-h" />
-                            <i class="fas fa-1" />
-                        </span>
-                    </li>
-                    <li>
-                        <span
-                            class="dropdown-item text-muted d-flex flex-row gap-1"
-                            @click="heading(2)"
-                        >
-                            <i class="fas fa-h" />
-                            <i class="fas fa-2" />
-                        </span>
-                    </li>
-                    <li>
-                        <span
-                            class="dropdown-item text-muted d-flex flex-row gap-1"
-                            @click="heading(3)"
-                        >
-                            <i class="fas fa-h" />
-                            <i class="fas fa-3" />
-                        </span>
-                    </li>
-                    <li>
-                        <span
-                            class="dropdown-item text-muted d-flex flex-row gap-1"
-                            @click="heading(4)"
-                        >
-                            <i class="fas fa-h" />
-                            <i class="fas fa-4" />
-                        </span>
-                    </li>
-                    <li>
-                        <span
-                            class="dropdown-item text-muted d-flex flex-row gap-1"
-                            @click="heading(5)"
-                        >
-                            <i class="fas fa-h" />
-                            <i class="fas fa-5" />
-                        </span>
-                    </li>
-                    <li>
-                        <span
-                            class="dropdown-item text-muted d-flex flex-row gap-1"
-                            @click="heading(6)"
-                        >
-                            <i class="fas fa-h" />
-                            <i class="fas fa-6" />
-                        </span>
-                    </li>
-                    <li>
-                        <span
-                            class="dropdown-item text-muted"
-                            @click="paragraph()"
-                        >
-                            <i class="fas fa-paragraph" />
-                        </span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <span
-            @click="bold()"
-        >
-            <i class="fas fa-fw fa-bold" />
-        </span>
-        <span
-            @click="emph()"
-        >
-            <i class="fas fa-fw fa-italic" />
-        </span>
-        <span
-            @click="strike()"
-        >
-            <i class="fas fa-fw fa-strikethrough" />
-        </span>
-        |
-        <span
-            @click="orderlist()"
-        >
-            <i class="fas fa-fw fa-list-ol" />
-        </span>
-        <span
-            @click="bulletlist()"
-        >
-            <i class="fas fa-fw fa-list-ul" />
-        </span>
-        <span
-            @click="indent_item()"
-        >
-            <i class="fas fa-fw fa-indent" />
-        </span>
-        |
-        <span
-            :class="{'opacity-50': state.editMode}"
-            @click="editmode()"
-        >
-            <i class="fab fa-fw fa-markdown" />
-        </span>
+            </span>
+
+            <separator
+                v-if="index !== commandGroups.length - 1"
+                :key="`separator-${index}`"
+                class="separator"
+            />
+        </template>
     </div>
 </template>
 
 <script>
+
 import {
     computed,
     reactive,
     toRefs,
 } from 'vue';
+
 import { useI18n } from 'vue-i18n';
 
 import {
@@ -150,17 +50,26 @@ import {
     wrapInBulletListCommand,
     wrapInOrderedListCommand,
     sinkListItemCommand,
+    liftListItemCommand,
 } from '@milkdown/preset-commonmark';
+
 import { toggleStrikethroughCommand } from '@milkdown/preset-gfm';
+
 import {
     redoCommand,
     undoCommand,
 } from '@milkdown/plugin-history';
+
 import {
     callCommand,
 } from '@milkdown/utils';
 
+import Separator from '@/components/structure/Separator.vue';
+
 export default {
+    components: {
+        Separator,
+    },
     props: {
         editor: {
             required: true,
@@ -174,45 +83,6 @@ export default {
             editor,
         } = toRefs(props);
 
-        // FUNCTIONS
-        const command = (action, data) => {
-            state.mde.action(callCommand(action, data));
-        }
-        const undo = _ => {
-            command(undoCommand.key);
-        };
-        const redo = _ => {
-            command(redoCommand.key);
-        };
-        const heading = i => {
-            command(wrapInHeadingCommand.key, i);
-        };
-        const paragraph = _ => {
-            command(turnIntoTextCommand.key);
-        };
-        const bold = _ => {
-            command(toggleStrongCommand.key);
-        };
-        const emph = _ => {
-            command(toggleEmphasisCommand.key);
-        };
-        const strike = _ => {
-            command(toggleStrikethroughCommand.key);
-        };
-        const orderlist = _ => {
-            command(wrapInOrderedListCommand.key);
-        };
-        const bulletlist = _ => {
-            command(wrapInBulletListCommand.key);
-        };
-        const indent_item = _ => {
-            command(sinkListItemCommand.key);
-        };
-        const editmode = _ => {
-            state.editMode = !state.editMode;
-            context.emit('toggle', state.editMode);
-        };
-
         // DATA
         const state = reactive({
             mde: computed(_ => {
@@ -221,27 +91,159 @@ export default {
             editMode: false,
         });
 
-        // ON MOUNTED
+        // FUNCTIONS
+        const command = (action, data) => {
+            state.mde.action(callCommand(action, data));
+        }
+
+        const heading = i => {
+            command(wrapInHeadingCommand.key, i);
+        };
+
+        const toggleEditmode = _ => {
+            state.editMode = !state.editMode;
+            context.emit('toggle', state.editMode);
+        };
+
+        // As we need to dynamically change classes
+        // it's most suitable to pass a computed prop.
+        // Therefore we need to extract the value on change
+        // so we use this helper function.
+        const getClassFromCommand = command => {
+            let cssClass = '';
+            if(command.class) {
+                cssClass = command.class;
+                if(cssClass.value) {
+                    cssClass = cssClass.value;
+                }
+            }
+
+            return cssClass;
+        };
+
+        const redoGroup = [
+            {
+                name: 'undo',
+                command: _ => command(undoCommand.key),
+                icon: 'fas fa-fw fa-undo',
+            }, {
+                name: 'redo',
+                command: _ => command(redoCommand.key),
+                icon: 'fas fa-fw fa-redo',
+            },
+        ];
+
+        const headingsGroup = [
+            {
+                name: 'heading',
+                command: _ => heading(1),
+                icon: ['fas fa-fw fa-h', 'fas fa-1'],
+
+            },
+            {
+                name: 'heading',
+                command: _ => heading(2),
+                icon: ['fas fa-fw fa-h', 'fas fa-2'],
+
+            },
+            {
+                name: 'heading',
+                command: _ => heading(3),
+                icon: ['fas fa-fw fa-h', 'fas fa-3'],
+
+            },
+            {
+                name: 'heading',
+                command: _ => heading(4),
+                icon: ['fas fa-fw fa-h', 'fas fa-4'],
+
+            },
+            {
+                name: 'paragraph',
+                command: _ => command(turnIntoTextCommand.key),
+                icon: 'fas fa-fw fa-paragraph',
+
+            },
+        ];
+
+        const stylingGroup = [
+            {
+                name: 'bold',
+                command: _ => command(toggleStrongCommand.key),
+                icon: 'fas fa-fw fa-bold',
+
+            },
+            {
+                name: 'italic',
+                command: _ => command(toggleEmphasisCommand.key),
+                icon: 'fas fa-fw fa-italic',
+
+            },
+            {
+                name: 'strikethrough',
+                command: _ => command(toggleStrikethroughCommand.key),
+                icon: 'fas fa-fw fa-strikethrough',
+
+            },
+        ];
+
+        const listGroup = [
+            {
+                name: 'orderlist',
+                command: _ => command(wrapInOrderedListCommand.key),
+                icon: 'fas fa-fw fa-list-ol',
+
+            },
+            {
+                name: 'bulletlist',
+                command: _ => command(wrapInBulletListCommand.key),
+                icon: 'fas fa-fw fa-list-ul',
+
+            },
+            {
+                name: 'outdent_item',
+                command: _ => command(liftListItemCommand.key),
+                icon: 'fas fa-fw fa-outdent',
+
+            },
+            {
+                name: 'indent_item',
+                command: _ => command(sinkListItemCommand.key),
+                icon: 'fas fa-fw fa-indent',
+
+            },
+        ];
+
+        const markdownClass = computed(_ => {
+            return state.editMode ? 'opacity-50' : '';
+        });
+
+        const utilsGroup = [
+            {
+                name: 'editmode',
+                command: _ => toggleEditmode(),
+                icon: 'fab fa-fw fa-markdown',
+                class: markdownClass
+            },
+        ]
+
+        const commandGroups = [
+            redoGroup,
+            headingsGroup,
+            stylingGroup,
+            listGroup,
+            utilsGroup,
+        ];
 
         // RETURN
         return {
             t,
-            // HELPERS
-            // PROPS
             // LOCAL
-            undo,
-            redo,
-            heading,
-            paragraph,
-            bold,
-            emph,
-            strike,
-            orderlist,
-            bulletlist,
-            indent_item,
-            editmode,
+            getClassFromCommand,
+            toggleEditmode,
             // STATE
             state,
+            commandGroups,
         };
     },
 }
