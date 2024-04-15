61157f1b2b381cd75f10a7a2e49bf3bb6ce9ce27
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2024-04-15 08:28:00 +0200

    fix deleting wrong dir in literature migration
    Signed-off-by: Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>

--- database/migrations/2022_11_17_114707_add_bibliography_file.php
+++ database/migrations/2022_11_17_114707_add_bibliography_file.php
@@ -46,7 +46,7 @@ return new class extends Migration
             $table->dropColumn('url');
         });
 
-        Storage::deleteDirectory('avatars');
+        Storage::deleteDirectory('bibliography');
 
         activity()->enableLogging();
     }
