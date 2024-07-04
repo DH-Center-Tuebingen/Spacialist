a3ec146eb8d7bd2efab31f1fa72de6e7afe9dd46
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2024-07-04 16:12:02 +0200

    add webgis dummy component
    Signed-off-by: Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>

---
+++ resources/js/components/openaccess/WebGis.vue
@@ -0,0 +1,40 @@
+<template>
+    <h4>
+        Here will be a GIS in the future!
+    </h4>
+</template>
+
+<script>
+    import {
+        computed,
+        reactive,
+    } from 'vue';
+
+    import { useI18n } from 'vue-i18n';
+
+    export default {
+        setup(props) {
+            const { t } = useI18n();
+
+            // FETCH
+
+            // DATA
+            const state = reactive({
+            });
+
+            // FUNCTIONS
+
+            // WATCHER
+
+            // RETURN
+            return {
+                t,
+                // HELPERS
+                // LOCAL
+                // PROPS
+                // STATE
+                state,
+            };
+        }
+    };
+</script>
