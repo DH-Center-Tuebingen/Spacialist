54e480bf47c2f0992c8f54b7d3172ebd1d0609e6
Severino <severin.opel@hotmail.com>
2025-02-05 10:54:51 +0100

    Fixed sql attribute thworing error when :entity_id is not present

--- app/AttributeTypes/SqlAttribute.php
+++ app/AttributeTypes/SqlAttribute.php
@@ -2,6 +2,8 @@
 
 namespace App\AttributeTypes;
 
+use Illuminate\Support\Facades\DB;
+
 class SqlAttribute extends StaticAttribute
 {
     protected static string $type = "sql";
@@ -14,5 +16,37 @@ class SqlAttribute extends StaticAttribute
 
     public static function serialize(mixed $data) : mixed {
         return false;
+    }
+    
+    
+    public static function execute($sql, $entityId) {
+        // if entity_id is referenced several times
+        // add an incrementing counter, so the
+        // references are unique (required by PDO)
+        $i = 0;
+        $safes = [];
+        $text = preg_replace_callback('/:entity_id/', function ($matches) use (&$i, &$safes) {
+            ++$i;
+            $safes[':entity_id_' . $i] = $entityId;
+            return $matches[0] . '_' . $i;
+        }, $sql);
+        
+        DB::beginTransaction();
+        $sqlValue = DB::select($text, $safes);
+        DB::rollBack();
+
+        // Check if only one result exists
+        if(count($sqlValue) === 1) {
+            // Get all column indices (keys) using the first row
+            $valueKeys = array_keys(get_object_vars($sqlValue[0]));
+            // Check if also only one key/column exists
+            if(count($valueKeys) === 1) {
+                // If only one row and one column exist,
+                // return plain value instead of array
+                $firstKey = $valueKeys[0];
+                $sqlValue = $sqlValue[0]->{$firstKey};
+            }
+        }
+        return $sqlValue;
     }
 }
