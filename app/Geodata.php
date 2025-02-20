ecfac9d53901d48aea70b0d112e40894cef1ff2f
Vinzenz Rosenkranz <v1r0x@users.noreply.github.com>
2025-02-20 08:04:11 +0100

    [0.11] Refactor geodata-package (#553)
    * Migrated from LaravelPostgis to Magellan geo package
    
    ---------
    
    Signed-off-by: Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
    Co-authored-by: Sev <captain.sev@outlook.com>

--- app/Geodata.php
+++ app/Geodata.php
@@ -8,10 +8,11 @@ use Clickbar\Magellan\IO\Generator\WKT\WKTGenerator;
 use Clickbar\Magellan\IO\Parser\WKB\WKBParser;
 use Clickbar\Magellan\IO\Parser\WKT\WKTParser;
 use Illuminate\Support\Facades\DB;
+use Exception;
 
 class Geodata
 {
-    protected static $availableGeometryTypes = [
+    protected static array $availableGeometryTypes = [
         'Point', 'LineString', 'Polygon', 'MultiPoint', 'MultiLineString', 'MultiPolygon'
     ];
 
@@ -19,23 +20,23 @@ class Geodata
         return self::$availableGeometryTypes;
     }
 
-    public static function fromWKB(string $wkb): Geometry {
-        $parser = app(WKBParser::class);
-        return $parser->parse($wkb);
+    public static function fromWKB(string $wkb): ?Geometry {
+        try {
+            $parser = app(WKBParser::class);
+            return $parser->parse($wkb);
+        } catch(Exception $e) {
+            return null;
+        }
     }
 
-    /**
-     * Parses a WKT string into a Geometry object
-     * if the WKT string is invalid, an exception is thrown.
-     *
-     * @param string $wkt
-     * @return Geometry
-     * @throws Clickbar\Magellan\Exception\UnknownWKTTypeException
-     */
-    public static function fromWKT(string $wkt): Geometry {
-        $parser = app(WKTParser::class);
-        // SRID=4326;POINT (2, 2)
-        return $parser->parse($wkt);
+    public static function fromWKT(string $wkt): ?Geometry {
+        try {
+            $parser = app(WKTParser::class);
+            // SRID=4326;POINT (2, 2)
+            return $parser->parse($wkt);
+        } catch(Exception $e) {
+            return null;
+        }
     }
 
     public static function toWKB(Geometry $geometry): string {
@@ -50,11 +51,11 @@ class Geodata
         return self::toWKB(self::fromWKT($wkt));
     }
 
-    public static function wkb2wkt(string $wkb) : string {
+    public static function wkb2wkt(string $wkb): string {
         return self::toWKT(self::fromWKB($wkb));
     }
 
-    public static function arrayToWKT(array $arr) : string {
+    public static function arrayToWKT(array $arr): string {
         $json = json_encode($arr);
         return DB::select("SELECT ST_AsText(ST_GeomFromGeoJSON('$json')) AS wkt")[0]->wkt;
     }
--- tests/Unit/GeodataTest.php
+++ tests/Unit/GeodataTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use Tests\TestCase;
 use App\Geodata;
 use Clickbar\Magellan\Data\Geometries\Dimension;
 use Clickbar\Magellan\Exception\UnknownWKTTypeException;
@@ -15,8 +16,8 @@ class GeodataTest extends TestCase
      * @return void
      */
     public function testFromWktWithTypo() {
-        $this->expectException(UnknownWKTTypeException::class);
-        Geodata::fromWKT('PIONT(1 2)');
+        $typoData = Geodata::fromWKT('PIONT(1 2)');
+        $this->assertNull($typoData);
     }
 
     /**
