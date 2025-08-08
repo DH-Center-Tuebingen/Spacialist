e1610823f94dcf648e1bfcdbf36759e89d6ff985
Severino <severin.opel@hotmail.com>
2025-08-08 11:27:45 +0200

    Improved getSubPath

--- app/Utils/UrlUtils.php
+++ app/Utils/UrlUtils.php
@@ -18,11 +18,20 @@ class UrlUtils {
      */
     static function getSubPath($url) : string {
         $matches = [];
+        // Get the string after the domain entry.
         $doesMatch = preg_match('/https?:\/\/.+?\/(.*)/', $url, $matches);
-        if(!$doesMatch || !isset($matches[1]) || empty(trim($matches[1]))) {
+        if(!$doesMatch || !isset($matches[1])) {
             return '/';
         }
-        $subpathUrl = trim($matches[1]);
-        return Str::start(trim($subpathUrl, '/'), '/');
+        $trimDefaultCharacters = " \n\r\t\v\0";
+        // Remove all irrelevant characters from the subpath
+        // including all leading and trailing slashes.
+        $subPathUrl = trim($matches[1], $trimDefaultCharacters . '/');
+        if(empty($subPathUrl)) {
+            return '/';
+        }
+
+        // Add leading slash to the subpath.
+        return  '/' . $subPathUrl;
     }
 }
