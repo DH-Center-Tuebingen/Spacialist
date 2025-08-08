e2281700cc9be6b94dcd876b86d3551f9a6e64ee
Severino <severin.opel@hotmail.com>
2025-08-08 10:48:47 +0200

    Improved getSubpath

--- app/Utils/UrlUtils.php
+++ app/Utils/UrlUtils.php
@@ -2,12 +2,19 @@
 
 namespace App\Utils;
 
+use Illuminate\Support\Str;
+
 class UrlUtils {
     /**
      * When serving multiple applications (also already spacialist + thesaurex)
      * at the same domain, we need to separate the CSRF cookies.
      * This function extracts the subpath from the app URL
      * and returns it.
+     *
+     * @param string $url
+     * @return string - Returns the subpath of the url with a leading '/'.
+     *                  If there is no subpath present, '/' will be returned.
+     *                  For Example https://spacialist.com/ancient-rome/ would result in '/ancient-rome'
      */
     static function getSubPath($url) : string {
         $matches = [];
@@ -15,6 +22,7 @@ class UrlUtils {
         if(!$doesMatch || !isset($matches[1]) || empty(trim($matches[1]))) {
             return '/';
         }
-        return '/' . ltrim(trim($matches[1]), '/'); // remove leading and trailing slashes
+        $subpathUrl = trim($matches[1]);
+        return Str::start(trim($subpathUrl, '/'), '/');
     }
 }
