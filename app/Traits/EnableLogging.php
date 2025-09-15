1849f1861185b29814acfe7989d09ce833996b04
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2025-09-15 10:17:46 +0200

    add description to empty trait

--- app/Traits/EnableLogging.php
+++ app/Traits/EnableLogging.php
@@ -2,6 +2,8 @@
 
 namespace App\Traits;
 
+// This empty trait is used in migrations to re-enable activitylog logging
+// It is handled in App\Listeners\DisableMigrationLogging
 trait EnableLogging
 {
 }
