44770ae265ed53f7ab91e1bd170c43c447fcef22
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2025-09-15 09:45:43 +0200

    fix missing listener on event; add trait to explicitly enable logging in migration

--- app/Listeners/DisableMigrationLogging.php
+++ app/Listeners/DisableMigrationLogging.php
@@ -2,6 +2,7 @@
 
 namespace App\Listeners;
 
+use App\Traits\EnableLogging;
 use Illuminate\Database\Events\MigrationStarted;
 use Illuminate\Database\Events\MigrationEnded;
 
@@ -17,6 +18,10 @@ class DisableMigrationLogging {
      * Handle the event.
      */
     public function handle(MigrationStarted | MigrationEnded $event): void {
+        // Do not disbale logging if EnableLogging trait is used
+        if(in_array(EnableLogging::class, class_uses($event->migration))) {
+            return;
+        }
         if($event instanceof MigrationStarted) {
             activity()->disableLogging();
         } else if($event instanceof MigrationEnded) {
