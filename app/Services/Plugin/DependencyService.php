a866bb4f10225f0d5041c784cdf68e00cd76925b
Severino <severin.opel@hotmail.com>
2026-08-13 16:52:29 +0200

    Refactor PluginUploader and DependencyService for improved dependency management; add Dependency class for manifest support; update language files for error messages; enhance plugin.js for better unregistering logic.

--- app/Services/Plugin/DependencyService.php
+++ app/Services/Plugin/DependencyService.php
@@ -3,12 +3,14 @@
 namespace App\Services\Plugin;
 
 use App\Exceptions\PluginLifecycleException;
+use App\Globals;
 use App\Models\Plugin\Dependencies;
 use App\Plugin;
 use App\Plugin\PluginManifest;
 use App\Support\BootstrapCache;
 use App\Support\Log\PluginLog;
-use Exception;
+use App\Support\Plugin\Dependency;
+
 
 /**
  * Simple dependency requirements for the Plugin System.
@@ -16,8 +18,9 @@ use Exception;
  * 
  * ```xml
  * <dependencies>
- *      <File />
- *      <Map />
+ *      <core min='0.12.0'>
+ *      <plugin>File</plugin>
+ *      <plugin>Map</plugin>
  *      ...
  * </dependencies>
  * ```
@@ -25,6 +28,9 @@ use Exception;
 class DependencyService extends PluginService {
 
     use BootstrapCache;
+    
+    private array $dependencyErrors = [];
+    private array $dependencies = [];
     
     public function getCacheName(): string
     {
@@ -38,13 +44,12 @@ class DependencyService extends PluginService {
     
     public function onBeforeInstall(Plugin $plugin, PluginManifest $manifest): void
     {
-        $missingDependencies = $this->missingDependencies($manifest);
-        if(!empty($missingDependencies)) {
-            $missingDependenciesList = implode(', ', $missingDependencies);
-            throw new PluginLifecycleException($plugin, __("Cannot install plugin :pluginName because the following dependencies are missing: :missingDependencies.",[
-                "pluginName" => $plugin->name,
-                "missingDependencies" => $missingDependenciesList,
-            ]));
+        [$error] = $this->evaluateDependencies($plugin, $manifest);
+        if($this->dependencyErrors != ''){
+            throw new PluginLifecycleException(
+                $plugin, 
+                __('Voraussetzungen für die Installation sind nicht erfüllt:\n:dependencyError', $dependencyErrors)
+            );
         }
     }
 
@@ -58,19 +63,22 @@ class DependencyService extends PluginService {
         foreach($dependencies as $dependency) {
             $dependsOn = $dependency['tag'] ?? null;
             if(!$dependsOn) {
-                PluginLog::for($plugin)->warning("Invalid dependency declaration in plugin manifest of {$manifest->getName()}. Missing 'tag' attribute.");
+                PluginLog::for($plugin)->warning('Invalid dependency declaration in plugin manifest of {$manifest->getName()}. Missing "tag" attribute.');
                 continue;
             }
             
-            $dependsOnPlugin = Plugin::where('name', $dependsOn)->first() ?? null;
-            if(!$dependsOnPlugin) {
-                throw new Exception("Plugin dependency '$dependsOn' not found for plugin '{$manifest->getName()}'. This should have been caught in the onBeforeInstall check.");
-            }
+            // Allows users to type the plugin in lowercase, or
+           
+                   
+            // $dependsOnPlugin = Plugin::where('name', $dependsOn)->first() ?? null;
+            // if(!$dependsOnPlugin) {
+            //     throw new Exception('Plugin dependency '$dependsOn' not found for plugin '{$manifest->getName()}'. This should have been caught in the onBeforeInstall check.');
+            // }
         
-            Dependencies::create([
-                'plugin_id' => $plugin->id,
-                'depends_on' => $dependsOnPlugin->id,
-            ]);
+            // Dependencies::create([
+            //     'plugin_id' => $plugin->id,
+            //     'depends_on' => $dependsOnPlugin->id,
+            // ]);
         }
     }
     
@@ -83,27 +91,115 @@ class DependencyService extends PluginService {
         return $manifest->getTagNodes('dependencies/*');
     }
     
-    private function missingDependencies(PluginManifest $manifest): array {
+    
+    /**
+     * Checks if all dependencies are met.
+     * 
+     * @param Plugin\PluginManifest $manifest
+     * @return array{ dependencies: Dependency[], errors: string[] } - Returns an array of the missing plugin names.
+     */
+    private function evaluateDependencies(Plugin $plugin, PluginManifest $manifest): array {
+        $errors = [];
         $dependencies = $this->getDependenciesFromManifest($manifest);
         if(empty($dependencies)) {
             return [];
         }
+    
+        [
+            'core' => $coreDependencies,
+            'plugin' => $pluginDependencies,
+            'unsupported' => $unsupportedDependencies,
+        ] = $this->decomposeDependencies($dependencies);
         
-        $missingDependencies = [];
+        $this->logWarningOfUnsupportedDependencies($plugin, $unsupportedDependencies);
+        $errors[] = $this->evaluatePluginDependencies($pluginDependencies);
+        $errors[] = $this->evaluateCoreDependencies($coreDependencies);
+
+        return [
+            'dependencies' => $dependencies,
+            'errors' => $errors,
+        ];
+    }
+    
+    /**
+     * Sorts the dependencies into three differnt buckets: core, plugins and unsupported.
+     */
+    private function decomposeDependencies(array $dependencies) : array{
+        $data = [
+            'core' => [],
+            'plugins' => [],
+            'unsupported' => [],
+        ];
         foreach($dependencies as $dependency) {
-            
-            if(!$dependency['tag'] || !is_string($dependency['tag'])) {
-                PluginLog::for($plugin)->warning("Invalid dependency declaration in plugin manifest of {$manifest->getName()}. Missing or invalid 'tag' attribute.");
-                continue;
-            }
+            switch($dependency['tag']) {
+                case 'plugin': $data['plugins'][] = new Dependency($dependency); break;
+                case 'core': $data['core'][] = new Dependency($dependency); break;
+                default: $data['unsupported'][] = new Dependency($dependency);
+            } 
+        }
         
-            $name = $dependency['tag'];
-            $plugin = Plugin::where('name', $name)->first();
-            if(!$plugin || !$plugin->installed_at) {
-                $missingDependencies[] = $name;
+        return $data;
+    }
+    
+    
+    /**
+     * Evaluates all unsupported dependencies and log them to the plugin log.
+     * 
+     * @param Plugin $plugin
+     * @param Dependency[] $dependencies List of plugin dependencies.
+     * @return void
+     */
+    private function logWarningOfUnsupportedDependencies(Plugin $plugin, array $dependencies): void {
+        if(count($dependencies) == 0) return;
+        $dependencyList = array_reduce($dependencies, function($carry, Dependency $dependency) {
+            $carry[] = "[{$dependency->tag}]";
+            return $carry;
+        } , []);
+        $dependencyText = implode(", ", $dependencyList);
+        PluginLog::for($plugin)->warning("Dependencies are not supporter and ignored: $dependencyText");
+    }
+    
+    /**
+     * Evaluates if the plugin dependencies are met:
+     * + Does the plugin exists
+     * + Does the plugin meet the specified version range
+     * 
+     * @param Dependency[] $dependencies List of plugin dependencies.
+     * @return string[] - Returns an array of errors messages encountered during evaliation.
+     */
+    private function evaluatePluginDependencies(array $dependencies): array{
+        $errors = [];
+        foreach($dependencies as $dependency) {
+            $requiredPlugin = Plugin::where('name', $dependency->name)->first();
+            if(!$requiredPlugin) {
+                $errors[] = __("Required Plugin is missing ':pluginName'");
+            } else if(!$requiredPlugin->installed_at) {
+                $errors[] = __("Required Plugin is not installed ':pluginName'");
+            } else {
+                $dependency->supportsVersion($requiredPlugin->version);
             }
         }
-
-        return $missingDependencies;
+        return $errors;
+    }
+    
+    /**
+     * Evaluates if the core dependencies are met:
+     * + Is there only a single core dependency
+     * + Is the core dependency in the version range.
+     * 
+     * @param Dependency[] $dependencies List of core dependencies.
+     * @return string[] - Returns an array of errors messages encountered during evaliation.
+     */
+    private function evaluateCoreDependencies(array $dependencies): array {
+        $errors = [];
+        if(count($dependencies) > 0){
+            $errors[] = __("Multiple core dependencies are present.");
+        } else {
+            $dependency = $dependencies[0];
+            if(!$dependency->supportsVersion(Globals::getVersion()['release'])){
+                $errors[] = __("Plugin is not compatible with the current Spacialist version.")
+            }
+        }
+        return $errors;
     }
 }
