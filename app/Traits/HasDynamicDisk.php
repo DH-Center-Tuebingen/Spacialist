f647373e30cba73f19bff1726d727caa093d01d1
Severino <captain.sev@outlook.com>
2026-09-11 14:02:19 +0200

    minor improvements

--- app/Traits/HasDynamicDisk.php
+++ app/Traits/HasDynamicDisk.php
@@ -2,7 +2,18 @@
 
 namespace App\Traits;
 
-
+/**
+ * Implements a $disk variable that allows the consumer to change the disk for
+ * special purposes.
+ * 
+ * Exposes:
+ * + getDisk(): string            - Disk getter function.
+ * + setDisk(string $disc): void  - Sets the disk.
+ * 
+ * 
+ * 
+ * Note: Primarily used to set a fake store inside tests.
+ */
 trait HasDynamicDisk {
     private $disk = "private";
     
